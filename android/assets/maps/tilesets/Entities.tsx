da40f105d9d961f429901ce56b6e194a6c9a632e
Edgar Silva <edgarasdaa10@gmail.com>
2022-07-23 15:37:34 +0100

    PathFinding update.

---
+++ android/assets/maps/tilesets/Entities.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Entities" tilewidth="50" tileheight="37" tilecount="3" columns="0">
+ <tile id="0">
+  <image width="50" height="37" source="../tiles/adventurer-idle-00.png"/>
+ </tile>
+ <tile id="1">
+  <image width="24" height="32" source="../tiles/idle0.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="25" source="../tiles/slime-idle-0.png"/>
+ </tile>
+</tileset>
