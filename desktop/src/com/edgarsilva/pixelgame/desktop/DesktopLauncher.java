aa94f8a34644cb4a74870c3afc16a9cbda6ecfe7
Edgar Silva <edgarasdaa10@gmail.com>
2022-07-23 15:37:35 +0100

    LoadingScreen and Loading animation added with assets management.

--- desktop/src/com/edgarsilva/pixelgame/desktop/DesktopLauncher.java
+++ desktop/src/com/edgarsilva/pixelgame/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.edgarsilva.pixelgame.PixelGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Cavel";
 		config.width  = (int) PixelGame.WIDTH;
 		config.height = (int) PixelGame.HEIGHT;
 		new LwjglApplication(new PixelGame(), config);
